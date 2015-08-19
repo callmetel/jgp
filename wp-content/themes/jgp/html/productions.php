92b291b54bc282af443e0a195e1c296e719bda92
Chantel <Tel@Chantels-MacBook-Pro.local>
2015-08-19 15:39:54 -0400

    Remove Store html

--- wp-content/themes/jgp/html/productions.php
+++ wp-content/themes/jgp/html/productions.php
@@ -10,48 +10,48 @@
 	<div id="bg-productions" class="subpage-bg">
 		<div class="wrap-subpg">
 
-			<?php include 'productions/productions-nav.php' ?>
+			<?php include 'productions/productions-nav.php'; ?>
 			
 			<header id="sub-pg-header" class="header fadeIn-scaleUp">
-				<?php include 'productions/header.php' ?>
+				<?php include 'productions/header.php'; ?>
 			</header>
 
 
 			<!-- Sections are pages within this page triggered with jquery -->
 
 			<section id="about-us" class="is-inactive">
-				<?php include 'productions/about/about-us.php' ?>
+				<?php include 'productions/about/about-us.php'; ?>
 			</section>
 
 			<section id="plays" class="is-inactive">
-				<?php include 'productions/plays/plays-grid.php' ?>
+				<?php include 'productions/plays/plays-grid.php'; ?>
 			</section>
 
 			<section id="auditions" class="is-inactive">
-				<?php include 'productions/auditions/booking-text.php' ?>
+				<?php include 'productions/auditions/booking-text.php'; ?>
 				<div class="divider"></div>
-				<?php include 'productions/auditions/booking-form.php' ?>
+				<?php include 'productions/auditions/booking-form.php'; ?>
 			</section>
 
 			<section id="tickets" class="is-inactive is-scrollable">
-				<?php include 'productions/tickets/tickets-list.php' ?>
-				<?php include 'productions/tickets/upcoming-events.php' ?>
-				<?php include 'productions/tickets/past-events.php' ?>
+				<?php include 'productions/tickets/tickets-list.php'; ?>
+				<?php include 'productions/tickets/upcoming-events.php'; ?>
+				<?php include 'productions/tickets/past-events.php'; ?>
 			</section>
 
 			<section id="classes" class="is-inactive is-scrollable">
-				<?php include 'productions/classes/class-list.php' ?>
+				<?php include 'productions/classes/class-list.php'; ?>
 			</section>
 		</div>
 		
-		<?php include 'partials/footer.php' ?>
+		<?php include 'partials/footer.php'; ?>
 	</div>
 
-	<?php include 'productions/plays/play-lightbox.php' ?>
-	<?php include 'productions/classes/class-lightbox.php' ?>
+	<?php include 'productions/plays/play-lightbox.php'; ?>
+	<?php include 'productions/classes/class-lightbox.php'; ?>
 	
-	<?php include 'productions/scripts.php' ?>
-	<?php include 'partials/script.php' ?>
+	<?php include 'productions/scripts.php'; ?>
+	<?php include 'partials/script.php'; ?>
 
 </body>
 </html>
