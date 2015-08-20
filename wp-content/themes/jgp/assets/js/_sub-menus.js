c716b4a9909bf2bedc4ae55b33f52e48386b9f10
Chantel <Tel@Chantels-MacBook-Pro.local>
2015-08-20 13:42:18 -0400

    Add submenus js

---
+++ wp-content/themes/jgp/assets/js/_sub-menus.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	//Define variables
+	
+	var
+		$menu_li = $('.menu-item'),
+		$menu_ul = $('.menu'),
+		$store_menu = $('#menu-heres2coolstuff-menu'),
+		$productions_menu = $('#menu-productions-menu'),
+		$studio_menu = $('#menu-jgpstudio-menu');
+
+			
+		if( $('ul').hasClass('menu')){
+			$menu_ul.addClass('nav-desktop');
+		} 
+
+		if( $('li').attr('#menu-item-110') ){
+			$menu_li.attr('id', 'menu-item-111');
+		} 
+
+});
