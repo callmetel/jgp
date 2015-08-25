b01df62aca90bbf6f5472d9565713933b081af1a
Chantel <Tel@Chantels-MacBook-Pro.local>
2015-08-25 13:08:16 -0400

    Implemented Plays Image Gallery & Solved js issue to link to my lightbox @jeffreynolte

--- wp-content/themes/jgp/assets/js/_lightbox.js
+++ wp-content/themes/jgp/assets/js/_lightbox.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
 
-		var $playLightbox = $('#play-lightbox'),
-			$classLightbox = $('#class-lightbox'),
-			$play1 = $('#play-1'),
-			$play2 = $('#play-2'),
-			$play3 = $('#play-3'),
-			$play4 = $('#play-4'),
-			$play5 = $('#play-5'),
-			$play6 = $('#play-6'),
-			$class1 = $('#class-1'),
-			$class2 = $('#class-2'),
-			$class3 = $('#class-3'),
-			$class4 = $('#class-4'),
-			$prevPlay = $('#prev-play'),
-			$class4 = $('#class-4'),
+	var $playLightbox = $('#play-lightbox'),
+	$classLightbox = $('#class-lightbox'),
+	$play1 = $('#play-1'),
+	$play2 = $('#play-2'),
+	$play3 = $('#play-3'),
+	$play4 = $('#play-4'),
+	$play5 = $('#play-5'),
+	$play6 = $('#play-6'),
+	$class1 = $('#class-1'),
+	$class2 = $('#class-2'),
+	$class3 = $('#class-3'),
+	$class4 = $('#class-4'),
+	$prevPlay = $('#prev-play'),
+	$galleryImage = $('a.play'),
+	$slider = $('div.slide-play'),
+	$close = $('.lightbox-close'),
 
 
-			$thankyou = $('#signup-thankyou');
+
+	$thankyou = $('#signup-thankyou');
+
+	$galleryImage.on('click', function () {
+		var $img_val = $(this).index() + 1;
+		
+		$slider.eq($img_val-1).removeClass('is-inactive');
+		$slider.eq($img_val-1).addClass('is-active');
+
+	});
+
+	$close.on('click', function(){
+		$slider.removeClass('is-active');
+		$slider.addClass('is-inactive');
+	})
 		// $submit.on('click', function() {
 		// 	$form.submit(function( event ) {
 		// 	  $thankyou.removeClass('is-inactive');
